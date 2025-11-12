bbac53b5df78c514512cbfd523969e19a5cc6f6f
VaibhavJaat421 <gaming2444555@gmail.com>
2025-11-13 03:24:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# config.py - PUBLIC BOT
 BOT_TOKEN = "7984133756:AAExRUlyH8Pyxm3YI143rbiAsAp8OTm3gng"
-ADMIN_CHAT_ID = 8185900627  # Your numeric chat ID
-DATABASE_PATH = "attack_bot.db"
-MAX_ATTACK_DURATION = 6000  # Reduced for public safety
+ADMIN_CHAT_ID = 8185900627
+MAX_DURATION = 300
 DEVELOPER = "@VAIBHAV_JAAT_OP"
-PUBLIC_MODE = True  # Anyone can use without authorization
